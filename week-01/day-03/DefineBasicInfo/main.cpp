55f566f72776a550d52b4a5fe807ddf77cb69c1a
Hordon13 <hordonandron@gmail.com>
2019-04-03 12:35:48 +0200

    Task: DefineBasicInfo

---
+++ week-01/day-03/DefineBasicInfo/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main(int argc, char* args[]) {
+
+    // Define several things as a variable then print their values
+    // Your name as a string
+    std::string name = "Donat";
+    std::cout << name << std::endl;
+    // Your age as an integer
+    int age = 24;
+    std::cout << age << std::endl;
+    // Your height in meters as a double
+    double height = 1.8;
+    std::cout << height << std::endl;
+    // Whether you are married or not as a boolean
+    bool merried = false;
+    std::cout << merried << std::endl;
+
+    return 0;
+}

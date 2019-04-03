a197d9c0598b6f9fc31e2c82252cc700095a626b
Hordon13 <hordonandron@gmail.com>
2019-04-03 14:52:20 +0200

    Task: CountFromTo

---
+++ week-01/day-03/CountFromTo/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+int main(int argc, char *args[]) {
+
+    // Create a program that asks for two numbers
+    // If the second number is not bigger than the first one it should print:
+    // "The second number should be bigger"
+    //
+    // If it is bigger it should count from the first number to the second by one
+    //
+    // example:
+    //
+    // first number: 3, second number: 6, should print:
+    //
+    // 3
+    // 4
+    // 5
+
+    int first;
+    int second;
+    std::cout << "First number?" << std::endl;
+    std::cin >> first;
+    std::cout << "Second number?" << std::endl;
+    std::cin >> second;
+
+    if (second <= first) {
+        std::cout << "The second number should be bigger" << std::endl;
+    } else {
+        for (first; first < second; first++) {
+            std::cout << first << std::endl;
+        }
+    }
+
+    return 0;
+}

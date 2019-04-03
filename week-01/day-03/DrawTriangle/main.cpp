80dc7cc72de2e0657a2b06063989047f49dc4d1a
Hordon13 <hordonandron@gmail.com>
2019-04-03 15:30:39 +0200

    Task: DrawTriangle

---
+++ week-01/day-03/DrawTriangle/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main(int argc, char *args[]) {
+
+    // Write a program that reads a number from the standard input, then draws a
+    // triangle like this:
+    //
+    // *
+    // **
+    // ***
+    // ****
+    //
+    // The triangle should have as many lines as the number was
+
+    int num;
+    std::cout << "Whats is your number?" << std::endl;
+    std::cin >> num;
+
+    for (int i = 0; i < num; ++i) {
+        for (int j = 0; j <= i; j++) {
+            std::cout << "*";
+        }
+        std::cout << "" << std::endl;
+    }
+
+    return 0;
+}

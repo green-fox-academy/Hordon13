145b85e09de05f4a36d44f7a2be3da8fab0c0268
Hordon13 <hordonandron@gmail.com>
2019-04-05 15:16:18 +0200

    Task: Unique - work in progress

--- week-01/day-04/Unique/main.cpp
+++ week-01/day-04/Unique/main.cpp
@@ -2,15 +2,16 @@
 #include <string>
 
 void unique(int[], int);
+void occurrence(int[], int);
 
-int main(int argc, char* args[]) {
+int main(int argc, char *args[]) {
 
     //  Create a function that takes a list of numbers as a parameter
     //  Don't forget you have pass the size of the list as a parameter as well
     //  Returns a list of numbers where every number in the list occurs only once
 
     //  Example
-    int numbers[] = {1, 11, 34, 11, 52, 61, 1, 34};
+    int numbers[] = {1, 11, 34, 11, 52, 61, 0, 1, 34, 1};
     int numSize = sizeof(numbers) / sizeof(int);
     unique(numbers, numSize);
 
@@ -19,18 +20,39 @@ int main(int argc, char* args[]) {
     return 0;
 }
 
-void unique(int ar[], int size){
+void unique(int ar[], int size) {
 
-    int collect[size];
+    int check[size];
+
+    //int newSize = 0;
 
     for (int i = 0; i < size; ++i) {
-        int num1 = ar[i];
-        for (int j = 1; j < size; ++j) {
-            int num2 = ar[j];
-            if(num1 != num2){
-                collect[i] = num1;
-                break;
+        int sum = 0;
+
+        for (int j = 0; j < size; ++j) {
+            if (ar[i] == ar[j] && i != j) {
+                //voltemar
+                //if
+                ++sum;
             }
         }
+        check[i] = sum;
     }
+
+
+    // Print
+    for (int k = 0; k < size; ++k) {
+        std::cout << check[k] << " ";
+    }
+    std::cout << "\n";
+
+    for (int k = 0; k < size; ++k) {
+        //std::cout << collect[k] << " ";
+    }
+    std::cout << "\n";
+}
+
+void occurrence(int arr[], int size){
+    
+
 }

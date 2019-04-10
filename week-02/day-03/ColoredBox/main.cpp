2c5a99f2fcd1123895baef929d499abf0c5027d9
Hordon13 <hordonandron@gmail.com>
2019-04-10 13:30:05 +0200

    Task: ColoredBox

--- week-02/day-03/ColoredBox/main.cpp
+++ week-02/day-03/ColoredBox/main.cpp
@@ -15,64 +15,72 @@ bool init();
 void close();
 
 //The window we'll be rendering to
-SDL_Window* gWindow = nullptr;
+SDL_Window *gWindow = nullptr;
 
 //The window renderer
-SDL_Renderer* gRenderer = nullptr;
+SDL_Renderer *gRenderer = nullptr;
 
-void draw()
-{
+void draw() {
     // Draw a box that has different colored lines on each edge.
     // The center of the box should align with the center of the screen.
+
+    SDL_SetRenderDrawColor(gRenderer, 255, 0, 0, 1);
+    SDL_RenderDrawLine(gRenderer, SCREEN_WIDTH / 4, SCREEN_HEIGHT / 4, (SCREEN_WIDTH * 3) / 4, SCREEN_HEIGHT / 4);
+
+    SDL_SetRenderDrawColor(gRenderer, 255, 255, 0, 1);
+    SDL_RenderDrawLine(gRenderer, (SCREEN_WIDTH * 3) / 4, SCREEN_HEIGHT / 4, (SCREEN_WIDTH * 3) / 4,
+                       (SCREEN_HEIGHT * 3) / 4);
+
+    SDL_SetRenderDrawColor(gRenderer, 0, 0, 255, 1);
+    SDL_RenderDrawLine(gRenderer, SCREEN_WIDTH / 4, (SCREEN_HEIGHT * 3) / 4, (SCREEN_WIDTH * 3) / 4,
+                       (SCREEN_HEIGHT * 3) / 4);
+
+    SDL_SetRenderDrawColor(gRenderer, 0, 255, 0, 1);
+    SDL_RenderDrawLine(gRenderer, SCREEN_WIDTH / 4, SCREEN_HEIGHT / 4, SCREEN_WIDTH / 4, (SCREEN_HEIGHT * 3) / 4);
+
 }
 
-bool init()
-{
+bool init() {
     //Initialize SDL
-    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-    {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         std::cout << "SDL could not initialize! SDL Error: " << SDL_GetError() << std::endl;
         return false;
     }
 
     //Create window
-    gWindow = SDL_CreateWindow( "Colored Box", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-    if( gWindow == nullptr )
-    {
+    gWindow = SDL_CreateWindow("Colored Box", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
+                               SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (gWindow == nullptr) {
         std::cout << "Window could not be created! SDL Error: " << SDL_GetError() << std::endl;
         return false;
     }
 
     //Create renderer for window
-    gRenderer = SDL_CreateRenderer( gWindow, -1, SDL_RENDERER_ACCELERATED );
-    if( gRenderer == nullptr )
-    {
+    gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
+    if (gRenderer == nullptr) {
         std::cout << "Renderer could not be created! SDL Error: " << SDL_GetError() << std::endl;
         return false;
     }
 
     //Initialize renderer color
-    SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
+    SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
 
     return true;
 }
 
-void close()
-{
+void close() {
     //Destroy window
-    SDL_DestroyRenderer( gRenderer );
-    SDL_DestroyWindow( gWindow );
+    SDL_DestroyRenderer(gRenderer);
+    SDL_DestroyWindow(gWindow);
     gWindow = nullptr;
     gRenderer = nullptr;
 
     SDL_Quit();
 }
 
-int main( int argc, char* args[] )
-{
+int main(int argc, char *args[]) {
     //Start up SDL and create window
-    if( !init() )
-    {
+    if (!init()) {
         std::cout << "Failed to initialize!" << std::endl;
         close();
         return -1;
@@ -85,7 +93,7 @@ int main( int argc, char* args[] )
     SDL_Event e;
 
     //While application is running
-    while( !quit ) {
+    while (!quit) {
         //Handle events on queue
         while (SDL_PollEvent(&e) != 0) {
             //User requests quit
@@ -99,7 +107,7 @@ int main( int argc, char* args[] )
         SDL_RenderClear(gRenderer);
 
         draw();
-        
+
         //Update screen
         SDL_RenderPresent(gRenderer);
     }

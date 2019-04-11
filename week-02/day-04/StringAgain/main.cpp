738272d6be2597a2890e69907e03423e0263b50f
Hordon13 <hordonandron@gmail.com>
2019-04-11 16:48:36 +0200

    Task: StringsAgain

---
+++ week-02/day-04/StringAgain/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+std::string change(std::string);
+
+int main() {
+
+    std::string input;
+    std::cout << "Give me a string: ";
+    std::cin >> input;
+
+    std::cout << change(input);
+
+    return 0;
+}
+
+std::string change(std::string in) {
+
+    if (in.size() < 1) {
+        return "";
+    }
+
+    int len = in.length() - 1;
+
+    if (in.at(len) == 'x') {
+        in.pop_back();
+        return change(in);
+    } else {
+        char temp = in.at(len);
+        in.pop_back();
+        return change(in) + temp;
+    }
+}

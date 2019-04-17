71b66e2bc316cdc59c80a77b0d46b2c39012452a
Hordon13 <hordonandron@gmail.com>
2019-04-17 15:53:07 +0200

    Task: Pokemon

---
+++ week-03/day-03/Pokemon/Pokemon.cpp
@@ -0,0 +1,30 @@
+#include "pokemon.h"
+
+Pokemon::Pokemon(const std::string& name, const std::string& type, const std::string& effectiveAgainst)
+{
+    _name = name;
+    _type = type;
+    _effectiveAgainst = effectiveAgainst;
+}
+
+const std::string &Pokemon::getName() const
+{
+    return _name;
+}
+
+const std::string &Pokemon::getType() const
+{
+    return _type;
+}
+
+const std::string &Pokemon::getEffectiveAgainst() const
+{
+    return _effectiveAgainst;
+}
+
+bool Pokemon::isEffectiveAgainst(Pokemon anotherPokemon)
+{
+
+    anotherPokemon.getType();
+    return _effectiveAgainst == anotherPokemon._type;
+}
---
+++ week-03/day-03/Pokemon/Pokemon.h
@@ -0,0 +1,23 @@
+#ifndef POKEMON_POKEMON_H
+#define POKEMON_POKEMON_H
+
+#include <iostream>
+
+class Pokemon {
+public:
+    Pokemon(const std::string& name, const std::string& type, const std::string& effectiveAgainst);
+    bool isEffectiveAgainst(Pokemon anotherPokemon);
+
+    const std::string &getName() const;
+
+    const std::string &getType() const;
+
+    const std::string &getEffectiveAgainst() const;
+
+private:
+    std::string _name;
+    std::string _type;
+    std::string _effectiveAgainst;
+};
+
+#endif //POKEMON_POKEMON_H

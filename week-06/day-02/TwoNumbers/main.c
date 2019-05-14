16fc30101874265f5983588dcc6324936d9c9e98
Hordon13 <hordonandron@gmail.com>
2019-05-14 11:49:28 +0200

    Task: Two Number

---
+++ week-06/day-02/TwoNumbers/main.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main()
+{
+    // Create a program that prints a few operations on two numbers: 22 and 13
+
+    // Print the result of 13 added to 22
+    int result1 = 13 + 12;
+    printf("%d\n", result1);
+
+    // Print the result of 13 substracted from 22
+    int result2 = 22 - 13;
+    printf("%d\n", result2);
+
+    // Print the result of 22 multiplied by 13
+    int result3 = 22 * 13;
+    printf("%d\n", result3);
+
+    // Print the result of 22 divided by 13 (as a decimal fraction)
+    int result4 = 22 / 13;
+    printf("%d\n", result4);
+
+    // Print the reminder of 22 divided by 13
+    int result5 = 22 % 13;
+    printf("%d\n", result5);
+
+    // Store the results in variables and use them when you display the result
+
+    return 0;
+}

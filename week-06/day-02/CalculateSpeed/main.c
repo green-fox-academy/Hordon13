56597fe629797afc18e2c92c1262457053467ee1
Hordon13 <hordonandron@gmail.com>
2019-05-14 16:10:28 +0200

    Task: Speed

---
+++ week-06/day-02/CalculateSpeed/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#define DISTANCE 345.0
+
+// fix the program so it prints out the correct result!
+
+int main()
+{
+    int seconds = 17;
+    float speed = DISTANCE / seconds;
+
+    printf("%f", speed);
+
+    return 0;
+}

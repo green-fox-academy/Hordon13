31fae34e3fdfacea612cdb1d41a4bb3649e481d5
Hordon13 <hordonandron@gmail.com>
2019-05-14 11:41:24 +0200

    Task: Hello Others

---
+++ week-06/day-02/HelloOthers/main.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    // Greet 3 of your classmates with this program, in three separate lines
+    // like:
+    //
+    // Hello, Esther!
+    // Hello, Mary!
+    // Hello, Joe!
+
+    puts("Hello, Esther!");
+    puts("Hello, Mary!");
+    puts("Hello, Joe!");
+
+    return 0;
+}

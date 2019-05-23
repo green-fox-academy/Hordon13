0f6d7662b63a352dd4b68023a9c3a897f5de8930
Hordon13 <hordonandron@gmail.com>
2019-05-23 16:57:54 +0200

    Task: BlockingTheProgram - finished

--- week-07/day-04/BlockingTheProgram/src/main.c
+++ week-07/day-04/BlockingTheProgram/src/main.c
@@ -2,7 +2,44 @@
 #include "stm32746g_discovery.h"
 #include "stm32f7xx_it.h"
 
+volatile int pressed = 0;
+
+void init_user_button()
+{
+  __HAL_RCC_GPIOI_CLK_ENABLE();
+
+  GPIO_InitTypeDef user_button;
+
+  user_button.Pin = GPIO_PIN_11;
+  user_button.Mode = GPIO_MODE_IT_RISING;
+
+  HAL_GPIO_Init(GPIOI, &user_button);
+  HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
+  HAL_NVIC_SetPriority(EXTI15_10_IRQn, 16, 0);
+
+}
+
 int main(void)
 {
-  
+  HAL_Init();
+  init_user_button();
+  BSP_LED_Init(LED1);
+
+  while (1) {
+    if(pressed){
+      BSP_LED_Toggle(LED1);
+      HAL_Delay(250);
+    }
+  }
+
+}
+
+void EXTI15_10_IRQHandler()
+{
+  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_11);
+}
+
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+  pressed = 1;
 }
